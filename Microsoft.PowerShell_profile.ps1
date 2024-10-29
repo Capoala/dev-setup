663127d57b2fd7ca3b983221455300e759236e90
Jonathan Nash <github@capoala.me>
2024-10-28 21:46:06 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,7 @@ $script:CIcon = [char]0xe61e         # nf-custom-c 
 $script:PythonIcon = [char]0xe73c    # nf-dev-python 
 $script:MarkdownIcon = [char]0xe73e  # nf-dev-markdown 
 $script:GitFolderIcon = [char]0xe5fb # nf-custom-folder_git 
-$script:TimerIcon = [char]0xdb80 + [char]0xdd09 # nf-md-camera_timer 󰄉
+$script:TimerIcon = [char]0xdb81 + [char]0xdd1b # nf-md-timer_outline 󰔛
 
 # Powerline
 $script:PowerlineRightRound = [char]0xE0B4
@@ -317,7 +317,7 @@ function Get-GitStats {
             TopLevel         = $topLevel
             Branch           = $branch
             Commit           = $commit
-            $Upstream        = $upstream
+            Upstream         = $upstream
             UntrackedFiles   = $untrackedFilesCount
             AddedFiles       = $addedFilesCount
             ModifiedFiles    = $modifiedFilesCount
